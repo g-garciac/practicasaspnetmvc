a9749c6e4157cef571e33512a25f419e7578e05b
Gerardo G <g.garciac@outlook.com>
2019-09-03 23:50:18 -0500

    Se agrega proyecto de POO

---
+++ DemoMvc/Practicas POO/Empleado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practicas_POO
+{
+    public class Empleado
+    {
+        public Empleado(string Id, string Nombre, decimal Sueldo)
+        {
+            this.Id = Id;
+            this.Nombre = Nombre;
+            this.Sueldo = Sueldo;
+        }
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Sueldo { get; private set; }
+
+        public void IncrementaSueldo(decimal porcentaje)
+        {
+            if (porcentaje <= 0 || porcentaje > 100)
+                throw new ArgumentException("El porcentaje no es válido");
+            Sueldo = Sueldo * (1 + porcentaje / 100);
+        }
+    }
+}
---
+++ DemoMvc/Practicas POO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practicas_POO
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var empleadoX = new Empleado("1234", "Luis", 10_000);
+
+            empleadoX.IncrementaSueldo(100_000);
+
+            Console.WriteLine(empleadoX.Sueldo);
+
+
+            //Dominio: modelar empleados mediante un identificador único, un nombre y un sueldo mensual;
+
+            //Ubiquitous Languaje
+
+        }
+    }
+}
